0602dc9d7f417673094b0792833a88ca172f4f49
benwaples <benwaples@gmail.com>
2021-03-17 11:19:15 -0700

    cups of milk

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
-birth_year = input('birth year: ')
-age = 2019 - birth_year
-#convert string to integer
-#can use float or bool for respective types
-int(birth_year)
-print(age)
+# birth_year = input('birth year: ')
+# age = 2019 - int(birth_year)
+# #convert string to integer
+# #can use float or bool for respective types
+# print(type(birth_year))
+# print(type(age))
+# print(age)
+
+cups_of_milk = input('how many cups of milk do you like: ')
+ounces = 8 * int(cups_of_milk)
+print(ounces)
