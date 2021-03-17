e1c2eb059c2820d651a4960a8b22791ba0e6ed48
benwaples <benwaples@gmail.com>
2021-03-17 13:45:24 -0700

    has_good_credit

--- app.py
+++ app.py
@@ -1,12 +1,21 @@
 is_hot = False
+is_cold = False
 
 
 # python using indentation instead of curly braces for code blocks
-if is_hot:
-    print("its a hot day")
-    print('drink waster')
-else:
-    print('its a cold day')
-    print('wear a jacket')
+# if is_hot:
+#     print("its a hot day")
+#     print('drink waster')
+# elif is_cold:
+#     print('its a cold day')
+#     print('wear a jacket')
+# else:
+#     print('enjoy your day')
 
-print('enjoy your day')
+has_good_credit = False
+home_cost = 1000000
+
+if has_good_credit:
+    print(f'down payment: {int(home_cost * .1)}')
+else:
+    print(f'down payment: {int(home_cost * .2)}')
