45d7db6e1a4d187557664f510f7ced40e15f4626
benwaples <benwaples@gmail.com>
2021-03-17 11:31:29 -0700

    formatted string

--- app.py
+++ app.py
@@ -1,16 +1,8 @@
-happy = 'i am happy'
+first = 'Ben'
+last = 'Waples'
 
-# python can get a -value for index and start from end of list
-print(happy[-2])
+# message = first + ' [' + last + '] is a coder'
 
-# this will print the characters from 0-3, excluding index 3 though
-print(happy[0:3])
-
-# print everything after the first index
-print(happy[1:])
-
-# print everthing before the 3rd index
-print(happy[:3])
-
-# make a copy of the happy string
-copy = happy[:]
+# formatted string
+msg = f'{first} [{last}] is a coder'
+print(msg)
