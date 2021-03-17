5a21a61672ea6cfc5ab8656926b1f3b8ab20f446
benwaples <benwaples@gmail.com>
2021-03-17 11:37:40 -0700

    methods

--- app.py
+++ app.py
@@ -6,3 +6,15 @@ last = 'Waples'
 # formatted string
 msg = f'{first} [{last}] is a coder'
 print(msg)
+
+string = 'hello my name is random'
+# len() to find length
+print(len(string))
+
+# we can still call methods on things, .upper() doesnt mutate original string
+print(string.upper())
+print(string)
+
+# still can find things, I wonder if not mutating things is common in py
+print(string.replace('o', 'ow'))
+print(string)
