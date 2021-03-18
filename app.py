6fa840793842849985a2e4bef507c8e0bc84a669
benwaples <benwaples@gmail.com>
2021-03-18 15:27:16 -0700

    start the car

--- app.py
+++ app.py
@@ -1,12 +1,24 @@
-secret_number = 3
-guesses = 0
+start_message = 'Car started... ready to go'
+stop_message = 'Car has stopped'
+help_message = '''
+say start to start your car
+say stop to stop your car
+say quit to quit game
+'''
+has_quit = False
 
-while guesses < 3:
-    user_guess = int(input('make a guess'))
-    if user_guess == secret_number:
-        print('you got it!')
-        break
-    if user_guess != secret_number and guesses == 2:
-        print('you lose')
-    guesses = guesses + 1
-print('game over')
+while not has_quit:
+    user_input = input('>')
+    if user_input == 'start':
+        print(start_message)
+    if user_input == 'stop':
+        print(stop_message)
+    if user_input == 'help':
+        print(help_message)
+    if user_input == 'quit':
+        print('game over')
+        has_quit = True
+    else:
+        print('idk wtf you mean')
+
+
