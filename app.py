4f7d0c382ec82bbda499113d10ba13e87e2939a5
benwaples <benwaples@gmail.com>
2021-03-18 15:14:01 -0700

    more game theory

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
-i = 1
+secret_number = 3
+guesses = 0
 
-while i <= 5:
-    print(i)
-    i = i + 1
-print('done')
+while guesses < 3:
+    user_guess = int(input('make a guess'))
+    if user_guess == secret_number:
+        print('you got it!')
+        break
+    if user_guess != secret_number and guesses == 2:
+        print('you lose')
+    guesses = guesses + 1
+print('game over')
