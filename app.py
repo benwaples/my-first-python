2101ddf001fdab1668d2d733ce84327b74762018
benwaples <benwaples@gmail.com>
2021-03-18 15:00:25 -0700

    / and //

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 weight = input('Weight: ')
 measurement = input('(L)bs or (k)g: ')
 
-if measurement == 'l':
+if measurement.upper() == 'L':
     print(f'you weigh {weight} pounds')
 else:
     print(f'you wiegh {weight} kilograms')
+
+# / will return a float, // will retrun a int
