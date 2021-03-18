a877c5e75fd8cd164354a6ff41e386e4a29a1041
benwaples <benwaples@gmail.com>
2021-03-18 14:57:11 -0700

    comparisons

--- app.py
+++ app.py
@@ -1,28 +1,7 @@
-is_hot = False
-is_cold = False
+weight = input('Weight: ')
+measurement = input('(L)bs or (k)g: ')
 
-
-# python using indentation instead of curly braces for code blocks
-# if is_hot:
-#     print("its a hot day")
-#     print('drink waster')
-# elif is_cold:
-#     print('its a cold day')
-#     print('wear a jacket')
-# else:
-#     print('enjoy your day')
-
-# has_good_credit = False
-# home_cost = 1000000
-#
-# if has_good_credit:
-#     print(f'down payment: {int(home_cost * .1)}')
-# else:
-#     print(f'down payment: {int(home_cost * .2)}')
-
-# and operator
-has_high_income = True
-has_good_credit = True
-
-if has_high_income and has_good_credit:
-    print('applicant is eligable')
+if measurement == 'l':
+    print(f'you weigh {weight} pounds')
+else:
+    print(f'you wiegh {weight} kilograms')
