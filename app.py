0096e6d222c1cf40dedf6a355eeeeaea3b109c8e
benwaples <benwaples@gmail.com>
2021-03-18 15:58:44 -0700

    my first python array!

--- app.py
+++ app.py
@@ -1,24 +1,5 @@
-start_message = 'Car started... ready to go'
-stop_message = 'Car has stopped'
-help_message = '''
-say start to start your car
-say stop to stop your car
-say quit to quit game
-'''
-has_quit = False
 
-while not has_quit:
-    user_input = input('>')
-    if user_input.lower() == 'start':
-        print(start_message)
-    if user_input.lower() == 'stop':
-        print(stop_message)
-    if user_input.lower() == 'help':
-        print(help_message)
-    if user_input.lower() == 'quit':
-        print('game over')
-        has_quit = True
-    else:
-        print('idk wtf you mean')
+myFirstArray = [1,2,3,4,5]
 
-
+for i in myFirstArray:
+    print(i)
